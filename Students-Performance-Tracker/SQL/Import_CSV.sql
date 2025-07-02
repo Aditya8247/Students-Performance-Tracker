56702d6a6a50a33d46c0311bb1e154180a189a28
Aditya <112696091+Aditya8247@users.noreply.github.com>
2025-07-02 17:34:16 +0530

    Import_CSV.sql

--- Students-Performance-Tracker/SQL/Import_CSV.sql
+++ Students-Performance-Tracker/SQL/Import_CSV.sql
@@ -1,5 +1,5 @@
 -- Replace '/path/to/file.csv' with your actual local file path
--- Alterbatively use workbench wizard to import data from csv file to tables(No query required)
+-- Alternatively use workbench wizard to import data from csv file to tables(No query required)
 
 LOAD DATA LOCAL INFILE 'C:/Users/HP/Downloads/Data_files/departments.csv'
 INTO TABLE departments
